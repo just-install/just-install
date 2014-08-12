58fc2da59f15a7de1825d3234c032aa5b476665a
Lorenzo Villani <lorenzo@villani.me>
2014-08-12 22:29:43 +0200

    Add icon back to the executable
    Resolves #41

--- build.cmd
+++ build.cmd
@@ -1,5 +1,11 @@
 set GOPATH=C:\go\gopath
 
 go build "just-install.go" || exit /b 1
+
 "%ProgramFiles%\Microsoft SDKs\Windows\v6.0A\bin\mt.exe" -manifest "just-install.exe.manifest" -outputresource:"just-install.exe;1" || exit /b 1
-misc\tools\upx.exe -9 just-install.exe || exit /b 1
+
+misc\tools\ResHacker.exe -add "just-install.exe", "just-install-tmp.exe", "misc\cube.ico", IconGroup, 1,
+del /Q "just-install.exe" || exit /b 1
+move "just-install-tmp.exe" "just-install.exe" || exit /b 1
+
+misc\tools\upx.exe --best "just-install.exe" || exit /b 1

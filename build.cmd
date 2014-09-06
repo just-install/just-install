04c12cf52c6e23de4b681ee91a0280165ab245a6
Lorenzo Villani <lorenzo@villani.me>
2014-09-06 15:30:12 +0200

    build.cmd: do not overwrite GOPATH env var

--- build.cmd
+++ build.cmd
@@ -1,5 +1,3 @@
-set GOPATH=C:\go\gopath
-
 go build "just-install.go" || exit /b 1
 
 "%ProgramFiles%\Microsoft SDKs\Windows\v6.0A\bin\mt.exe" -manifest "just-install.exe.manifest" -outputresource:"just-install.exe;1" || exit /b 1

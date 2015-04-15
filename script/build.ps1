9810413092b06d1339d8bc0b34457461367bdf34
Lorenzo Villani <lorenzo@villani.me>
2015-04-15 15:13:53 +0200

    Let's go back to WiX since AdvancedInstaller ins't free anymore

--- script/build.ps1
+++ script/build.ps1
@@ -1,9 +1,7 @@
 $HERE = Split-Path -Parent $MyInvocation.MyCommand.Definition
 $TOP_LEVEL = Split-Path -Parent $HERE
 
-$VERSION = "2.3.1"
-$TIMESTAMP = (Get-Date -Format "yyyyMMdHHmm")
-$BUILD = "$VERSION.$TIMESTAMP"
+$env:JustInstallVersion = "2.4.0"
 
 cd $TOP_LEVEL
 
@@ -11,25 +9,21 @@ cd $TOP_LEVEL
 # Clean
 #
 
-Remove-Item -Force just-install.000 | Out-Null
-Remove-Item -Force just-install.exe | Out-Null
-Remove-Item -Force just-install.msi | Out-Null
-Remove-Item -Recurse -Force deploy/just-install-cache | Out-Null
+Remove-Item -ErrorAction SilentlyContinue -Force just-install.exe
+Remove-Item -ErrorAction SilentlyContinue -Force just-install.msi
+Remove-Item -ErrorAction SilentlyContinue -Force just-install.txt
+Remove-Item -ErrorAction SilentlyContinue -Force just-install.wixobj
+Remove-Item -ErrorAction SilentlyContinue -Force just-install.wixpdb
 
 #
 # Build
 #
 
-go build just-install.go
+go build -o just-install.exe just-install.go
 
 #
-# Deploy
+# Build MSI
 #
 
-& "${env:ProgramFiles(x86)}\Windows Kits\8.1\bin\x86\mt.exe" -manifest deploy/just-install.exe.manifest -outputresource:"just-install.exe;1"
-& "${env:ProgramFiles(x86)}\UPX\upx391w\upx.exe" --best just-install.exe
-
-pushd deploy
-    AdvancedInstaller.com /edit just-install.aip /SetVersion "$BUILD"
-    AdvancedInstaller.com /rebuild just-install.aip
-popd
+candle just-install.wxs
+light just-install.wixobj

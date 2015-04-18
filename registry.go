7750807be06405eebfae031ad7b8eaef61516ce1
Lorenzo Villani <lorenzo@villani.me>
2015-04-18 12:28:49 +0200

    Move utilities to util.go

--- registry.go
+++ registry.go
@@ -1,25 +1,16 @@
 package justinstall
 
 import (
-	"archive/zip"
 	"encoding/json"
 	"errors"
 	"fmt"
-	"hash/crc32"
-	"io"
 	"io/ioutil"
 	"log"
-	"net/http"
-	"net/url"
 	"os"
-	"os/exec"
 	"path/filepath"
 	"sort"
-	"strconv"
 	"strings"
-	"time"
 
-	"github.com/cheggaaa/pb"
 	"github.com/ungerik/go-dry"
 )
 
@@ -309,154 +300,6 @@ func (e *registryEntry) CreateShims() {
 			log.Printf("Creating shim for %s (%s)\n", shimTarget, shim)
 
 			system(exeproxy, "exeproxy-copy", shim, shimTarget)
-		}
-	}
-}
-
-//
-// Utilities
-//
-
-func system(args ...string) {
-	var cmd *exec.Cmd
-
-	if len(args) == 0 {
-		return
-	} else if len(args) == 1 {
-		cmd = exec.Command(args[0])
-	} else {
-		cmd = exec.Command(args[0], args[1:]...)
-	}
-
-	log.Println("Running", strings.Join(args, " "))
-
-	err := cmd.Run()
-	if err != nil {
-		log.Fatalf(err.Error())
-	}
-}
-
-func copyFile(src string, dst string) error {
-	buf, err := ioutil.ReadFile(src)
-	if err != nil {
-		log.Fatalln(err.Error())
-	}
-
-	return ioutil.WriteFile(dst, buf, 0)
-}
-
-// Convenience wrapper over download3 which passes an empty ("") `ext` parameter.
-func download2(rawurl string, force bool) string {
-	return download3(rawurl, "", force)
-}
-
-// Downloads a file over HTTP(S) to a temporary location. The temporary file has a name derived
-// from the CRC32 of the URL string with the original file extension attached (if any). If `ext`
-// is not the empty string, it will be appended to the destination file. The file is re-downloaded
-// only if the temporary file is missing or `force` is true.
-func download3(rawurl string, ext string, force bool) string {
-	u, err := url.Parse(rawurl)
-	if err != nil {
-		log.Fatalf("Unable to parse the URL: %s", rawurl)
-	}
-
-	var base string
-
-	if ext != "" {
-		base = crc32s(rawurl) + ext
-	} else {
-		base = crc32s(rawurl) + filepath.Ext(u.Path)
-	}
-
-	dest := filepath.Join(os.TempDir(), base)
-
-	if !dry.FileExists(dest) || force {
-		download(rawurl, dest)
-	}
-
-	return dest
-}
-
-// Computes and returns the CRC32 of a string as an HEX string.
-func crc32s(s string) string {
-	crc32 := crc32.NewIEEE()
-	crc32.Write([]byte(s))
-
-	return fmt.Sprintf("%X", crc32.Sum32())
-}
-
-// Downloads a file with the HTTP/HTTPS protocol showing a progress bar. The destination file is
-// always overwritten.
-func download(rawurl string, destinationPath string) {
-	destination, err := os.Create(destinationPath)
-	if err != nil {
-		log.Fatalf("Unable to open the destination file: %s", destinationPath)
-	}
-	defer destination.Close()
-
-	response, err := http.Get(rawurl)
-	if err != nil {
-		log.Fatalf("Unable to open a connection to %s", rawurl)
-	}
-	defer response.Body.Close()
-
-	if response.StatusCode != http.StatusOK {
-		log.Fatalf("Unexpected HTTP response code. Wanted 200 but got %d", response.StatusCode)
-	}
-
-	var progressBar *pb.ProgressBar
-
-	contentLength, err := strconv.Atoi(response.Header.Get("Content-Length"))
-	if err == nil {
-		progressBar = pb.New(int(contentLength))
-	} else {
-		progressBar = pb.New(0)
-	}
-	defer progressBar.Finish()
-
-	progressBar.ShowSpeed = true
-	progressBar.SetRefreshRate(time.Millisecond * 1000)
-	progressBar.SetUnits(pb.U_BYTES)
-	progressBar.Start()
-
-	writer := io.MultiWriter(destination, progressBar)
-
-	io.Copy(writer, response.Body)
-}
-
-func extractZip(path string, extractTo string) {
-	os.MkdirAll(extractTo, 0700)
-
-	// Open the archive for reading
-	zipReader, err := zip.OpenReader(path)
-	if err != nil {
-		log.Fatalln("Unable to open ZIP archive:", path)
-	}
-	defer zipReader.Close()
-
-	// Extract all entries in the archive
-	for _, zipFile := range zipReader.File {
-		destinationPath := filepath.Join(extractTo, zipFile.Name)
-
-		if zipFile.FileInfo().IsDir() {
-			os.MkdirAll(destinationPath, zipFile.Mode())
-		} else {
-			// Create destination file
-			dest, err := os.Create(destinationPath)
-			if err != nil {
-				log.Fatalln("Unable to create destination:", destinationPath)
-			}
-			defer dest.Close()
-
-			// Open input stream
-			source, err := zipFile.Open()
-			if err != nil {
-				log.Fatalln("Unable to open input ZIP file:", zipFile.Name)
-			}
-			defer source.Close()
-
-			// Extract file
-			io.Copy(dest, source)
 		}
 	}
 }

47c619315fd5f0f9a89d3579afb8b7ed402958c4
Lorenzo Villani <lorenzo@villani.me>
2015-07-13 15:28:21 +0200

    Ensure that we are getting binaries and improve test output

--- registry_test.go
+++ registry_test.go
@@ -1,13 +1,25 @@
 package justinstall
 
 import (
-	"fmt"
+	"errors"
 	"net/http"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/ungerik/go-dry"
 	"github.com/xeipuuv/gojsonschema"
 )
+
+var expectedContentTypes = []string{
+	"application/octet-stream",
+	"application/x-msdos-program",
+	"application/x-msdownload",
+	"application/x-msi",
+	"application/x-zip-compressed",
+	"application/zip",
+	"Composite Document File V2 Document, corrupt: Can't read SAT; charset=binary",
+	"Zip Files",
+}
 
 func TestValidRegistry(t *testing.T) {
 	schemaLoader := gojsonschema.NewReferenceLoader("file://./just-install-schema.json")
@@ -25,18 +37,25 @@ func TestRegistryReachableLinks(t *testing.T) {
 		t.Skip("Skipping reachability test in short mode")
 	}
 
-	var errors []string
-
+	hasErrors := false
 	registry := SmartLoadRegistry(false)
 
-	checkLink := func(rawurl string) bool {
-		response, err := http.Get(rawurl)
+	checkLink := func(rawurl string) error {
+		response, err := customGet(rawurl)
 		if err != nil {
-			return false
+			return err
 		}
 		defer response.Body.Close()
 
-		return response.StatusCode == http.StatusOK
+		if response.StatusCode != http.StatusOK {
+			return errors.New("Status code wasn't 200 OK")
+		}
+
+		if !dry.StringInSlice(response.Header.Get("Content-Type"), expectedContentTypes) {
+			return errors.New("The content type was " + response.Header.Get("Content-Type"))
+		}
+
+		return nil
 	}
 
 	checkArch := func(name string, entry *registryEntry, architecture string, rawUrl string) {
@@ -45,9 +64,9 @@ func TestRegistryReachableLinks(t *testing.T) {
 		}
 
 		url := entry.expandString(rawUrl)
-
-		if !checkLink(url) {
-			errors = append(errors, fmt.Sprintf("%v (%v): %v", name, architecture, url))
+		if err := checkLink(url); err != nil {
+			t.Logf("%v (%v): %v, %v", name, architecture, url, err)
+			hasErrors = true
 		}
 	}
 
@@ -58,5 +77,5 @@ func TestRegistryReachableLinks(t *testing.T) {
 		checkArch(name, &entry, "x86_64", entry.Installer.X86_64)
 	}
 
-	assert.Empty(t, errors)
+	assert.False(t, hasErrors)
 }

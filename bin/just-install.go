e2427ba45f9d2422cf7740661058bd07790a88e9
Lorenzo Villani <lorenzo@villani.me>
2017-10-17 16:28:47 +0200

    Add "audit" command

--- bin/just-install.go
+++ bin/just-install.go
@@ -24,12 +24,14 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"net/http"
 	"os"
 	"strings"
+	"time"
 
 	"github.com/codegangsta/cli"
-	"github.com/kardianos/osext"
 	"github.com/just-install/just-install"
+	"github.com/kardianos/osext"
 	dry "github.com/ungerik/go-dry"
 )
 
@@ -45,6 +47,10 @@ func main() {
 	app.Version = version
 
 	app.Commands = []cli.Command{{
+		Name:   "audit",
+		Usage:  "Audit the registry",
+		Action: handleAuditAction,
+	}, {
 		Name:   "clean",
 		Usage:  "Remove caches and temporary files",
 		Action: handleCleanAction,
@@ -157,6 +163,112 @@ func handleArguments(c *cli.Context) {
 		} else {
 			log.Println("WARNING: Unknown package", pkg)
 		}
+	}
+}
+
+func handleAuditAction(c *cli.Context) {
+	expectedContentTypes := []string{
+		"application/octet-stream",
+		"application/unknown", // Bintray
+		"application/x-dosexec",
+		"application/x-msdos-program",
+		"application/x-msdownload",
+		"application/x-msi",
+		"application/x-sdlc", // Oracle
+		"application/x-zip-compressed",
+		"application/zip",
+		"binary/octet-stream",
+		"Composite Document File V2 Document, corrupt: Can't read SAT; charset=binary", // Google Code
+		"text/x-python", // PIP
+		"Zip Files",
+	}
+
+	var collectedErrors []error
+
+	// FIXME: this chunk of code is duplicated with handleArguments().
+	var registry justinstall.Registry
+	if c.IsSet("registry") {
+		if !dry.FileExists(c.String("registry")) {
+			log.Fatalf("%v: no such file.\n", c.String("registry"))
+		}
+
+		registry = justinstall.LoadRegistry(c.String("registry"))
+	} else {
+		registry = justinstall.SmartLoadRegistry(false)
+	}
+
+	checkLink := func(rawurl string) error {
+		response, err := justinstall.CustomGet(rawurl)
+		if err != nil {
+			return err
+		}
+		defer response.Body.Close()
+
+		if response.StatusCode != http.StatusOK {
+			return fmt.Errorf("Status code: expected 200, got %v", response.StatusCode)
+		}
+
+		contentType := response.Header.Get("Content-Type")
+
+		// Exception: VirtualBox Extension Pack has the wrong MIME type
+		success := strings.HasSuffix(rawurl, ".vbox-extpack") && contentType == "text/plain"
+
+		// Exception: Some LibreOffice mirror returns the wrong MIME type
+		success = success || strings.Contains(rawurl, "libreoffice") && contentType == "application/x-troff-man"
+
+		success = success || dry.StringInSlice(contentType, expectedContentTypes)
+		if !success {
+			return errors.New("The content type was " + contentType)
+		}
+
+		return nil
+	}
+
+	retryCheckLink := func(rawurl string) error {
+		const retryDelay = 60
+		var err error
+
+		for i := 0; i < 3; i++ {
+			err = checkLink(rawurl)
+			if err == nil {
+				return nil
+			}
+
+			log.Printf("Failed check for %v, retrying in %v seconds...\n", rawurl, retryDelay)
+			time.Sleep(retryDelay * time.Second)
+		}
+
+		return err
+	}
+
+	checkArch := func(name string, entry *justinstall.RegistryEntry, architecture string, rawUrl string) {
+		if rawUrl == "" {
+			return
+		}
+
+		url := entry.ExpandString(rawUrl)
+		if err := retryCheckLink(url); err != nil {
+			collectedErrors = append(collectedErrors, err)
+		}
+	}
+
+	for _, name := range registry.SortedPackageNames() {
+		log.Println(name)
+
+		entry := registry.Packages[name]
+
+		checkArch(name, &entry, "x86", entry.Installer.X86)
+		checkArch(name, &entry, "x86_64", entry.Installer.X86_64)
+	}
+
+	if collectedErrors != nil {
+		log.Println("Found errors:")
+
+		for _, err := range collectedErrors {
+			log.Println(err)
+		}
+
+		os.Exit(1)
 	}
 }
 

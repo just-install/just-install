f57c9549b5f5732ff0ab5a7c167bd112be66b15a
Patrick G <geek1011@outlook.com>
2018-10-10 17:48:38 -0400

    Improved error checking in build script

--- make.go
+++ make.go
@@ -34,32 +34,24 @@ func clean() {
 	globs := []string{"*.exe", "*.msi", "*.wixobj", "*.wixpdb"}
 	for _, glob := range globs {
 		found, err := filepath.Glob(glob)
-		if err != nil {
-			log.Fatalln("cannot glob files to remove:", err)
-		}
-
+		checkErr(err, "cannot glob files to remove")
 		toRemove = append(toRemove, found...)
 	}
 
 	for _, path := range toRemove {
 		log.Println("deleting", path)
-		if err := os.RemoveAll(path); err != nil {
-			log.Fatalf("cannot remove %s: %s\n", path, err)
-		}
+		checkErr(os.RemoveAll(path), "cannot remove %s", path)
 	}
 }
 
 func build() {
 	log.Println("building version", getVersion())
-
 	cmd := exec.Command("go", "build",
 		"-ldflags", fmt.Sprintf("-s -w -X main.version=%s", getVersion()),
 		"-mod=readonly",
 		"./cmd/just-install")
 	cmd.Env = append(os.Environ(), "GOARCH=386")
-	if err := cmd.Run(); err != nil {
-		log.Fatalln("cannot build just-install:", err)
-	}
+	checkErr(cmd.Run(), "cannot build just-install")
 }
 
 func buildMsi() {
@@ -74,15 +66,11 @@ func buildMsi() {
 
 	cmd := exec.Command("candle", "just-install.wxs")
 	cmd.Env = env
-	if err := cmd.Run(); err != nil {
-		log.Fatalln("cannot build MSI installer:", err)
-	}
+	checkErr(cmd.Run(), "cannot build MSI installer")
 
 	cmd = exec.Command("light", "just-install.wixobj")
 	cmd.Env = env
-	if err := cmd.Run(); err != nil {
-		log.Fatalln("cannot link MSI installer:", err)
-	}
+	checkErr(cmd.Run(), "cannot link MSI installer")
 }
 
 func deploy() {
@@ -101,9 +89,7 @@ func deploy() {
 
 func createDeployArchive() {
 	f, err := os.Create("deploy.zip")
-	if err != nil {
-		log.Fatalln("cannot create deployment archive:", err)
-	}
+	checkErr(err, "cannot create deployment archive")
 	defer f.Close()
 
 	w := zip.NewWriter(f)
@@ -119,51 +105,39 @@ func createDeployArchive() {
 
 func zipCopy(w *zip.Writer, path string) {
 	dst, err := w.Create(path)
-	if err != nil {
-		log.Fatalf("cannot create zip writer for entry %v: %v\n", path, err)
-	}
+	checkErr(err, "cannot create zip writer for entry %s", path)
 
 	src, err := os.Open(path)
-	if err != nil {
-		log.Fatalf("cannot open %v for read: %v\n", path, err)
-	}
+	checkErr(err, "cannot open %s for reading", path)
 	defer src.Close()
 
 	if _, err := io.Copy(dst, src); err != nil {
-		log.Fatalf("cannot copy %v to deployment archive: %v\n", path, err)
+		checkErr(err, "cannot copy %s to deployment archive", path)
 	}
 }
 
 func zipWriteString(w *zip.Writer, path string, s string) {
 	dst, err := w.Create(path)
-	if err != nil {
-		log.Fatalf("cannot create zip writer for entry %v: %v\n", path, err)
-	}
+	checkErr(err, "cannot create zip writer for entry %s", path)
 
 	if _, err := dst.Write([]byte(s)); err != nil {
-		log.Fatalf("cannot write string as entry %v: %v\n", path, err)
+		checkErr(err, "cannot write string as entry %s", path)
 	}
 }
 
 func uploadDeployArchive(url string) {
 	f, err := os.Open("deploy.zip")
-	if err != nil {
-		log.Fatalln("cannot open deploy archive:", err)
-	}
+	checkErr(err, "cannot open deploy archive")
 	defer f.Close()
 
 	req, err := http.NewRequest("POST", url, f)
-	if err != nil {
-		log.Fatalln("cannot create request to Netlify's endpoint:", err)
-	}
+	checkErr(err, "cannot create request to netlify")
 
 	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", dry.EnvironMap()["NETLIFY_DEPLOY_TOKEN"]))
 	req.Header.Add("Content-Type", "application/zip")
 
 	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		log.Fatalln("request to Netlify upload endpoint failed:", err)
-	}
+	checkErr(err, "upload to netlify failed")
 
 	if resp.StatusCode != http.StatusOK {
 		log.Fatalln("expected 200 OK, instead got", resp.StatusCode)
@@ -176,9 +150,7 @@ func getVersion() string {
 	}
 
 	f, err := dry.FileGetJSON(".releng.json")
-	if err != nil {
-		log.Fatalln("cannot read .releng.json:", err)
-	}
+	checkErr(err, "cannot read .releng.json")
 
 	ret, ok := f.(map[string]interface{})["version"]
 	if !ok {
@@ -196,4 +168,10 @@ func shouldDeploy() bool {
 func isStableBuild() bool {
 	val, ok := dry.EnvironMap()["APPVEYOR_REPO_TAG_NAME"]
 	return ok && len(val) > 0
+}
+
+func checkErr(err error, message string, v ...interface{}) {
+	if err != nil {
+		log.Fatalf("%s: %v\n", fmt.Sprintf(message, v...), err)
+	}
 }

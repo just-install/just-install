83d6cc9b803da9eb2568b059a8bd8fbd5e3900d4
Lorenzo Villani <lorenzo@villani.me>
2020-02-26 19:46:21 +0100

    Update copyright

--- cmd/just-install/main.go
+++ cmd/just-install/main.go
@@ -1,5 +1,5 @@
 // just-install - The simple package installer for Windows
-// Copyright (C) 2019 just-install authors.
+// Copyright (C) 2020 just-install authors.
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
